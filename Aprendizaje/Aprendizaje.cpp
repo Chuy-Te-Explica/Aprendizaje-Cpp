aaf33b62d7f4dec2035dc2fbe6cfa55f69ee7f02
Noel de Jesus <Noel de Jesus@DESKTOP-6BG20T5>
2026-05-08 17:22:45 -0600

    Agregar archivos de proyecto.

---
+++ Aprendizaje/Aprendizaje.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream> // permite usar entrada/salida.
+
+// using namespace std; te evita escribir: std::cout. Pero en la practica no es recomendable.
+
+int main() { // es el punto de inicio del programa. Todo programa C++ empieza ahí.
+
+	std::cout << "¡Hola mundo!";
+
+	return 0;
+}
