fa2fdb743df63b723ce62c62eb47a6768ce9386f
Chuy Te Explica <133062999+Chuy-Te-Explica@users.noreply.github.com>
2026-05-08 19:54:52 -0600

    Update and rename Aprendizaje.cpp to Problema 0.cpp

---
+++ Aprendizaje/Problema 0.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // permite usar entrada/salida.
+
+// using namespace std; te evita escribir: std::cout. Pero en la practica no es recomendable.
+
+int main() { // es el punto de inicio del programa. Todo programa C++ empieza ahí.
+
+	std::cout << "¡Hola mundo!" << std::endl; // imprime el texto y realiza un salto de línea liberando el buffer.
+
+	return 0; // retorna 0 para confirmar que el programa se a ejecutado correctamente
+}
